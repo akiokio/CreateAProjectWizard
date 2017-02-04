f2dfc107e03c31c085a186a52edd10a2ca75d216
akiokio <akio.xd@gmail.com>
2017-02-03 22:00:32 -0800

    Menu enhancements and step 2 add basic functionality

--- js/global.js
+++ js/global.js
@@ -1,8 +1,9 @@
 (() => {
   const progress = document.querySelector('.progress');
-
   const updateProgress = (currentIndex) => {
-    progress.value = (currentIndex / 5) * 100;
+    progress.value = (currentIndex / 4) * 100;
+    document.querySelectorAll('.step').forEach((s) => s.classList.remove('is-active'));
+    const activeMenuItem = document.querySelector(`.step-${currentIndex}`).classList.add('is-active');
   };
   const mySiema = new Siema({
     draggable: false,
@@ -12,4 +13,15 @@
   });
   document.querySelector('.prev').addEventListener('click', () => mySiema.prev(updateProgress));
   document.querySelector('.next').addEventListener('click', () => mySiema.next(updateProgress));
+
+  const addColabButton = document.querySelector('.addColaborator');
+  const colaboratorsList = document.querySelector('.colaborators-list');
+  addColabButton.addEventListener('click', () => {
+    const colaboratorEmail = document.querySelector('#newColaboratorEmail');
+    const colaboratorNotify = document.querySelector('#shouldNotifyNewColaborator');
+    if (colaboratorEmail.value === '') { return alert('Cannot be blank'); }
+    colaboratorsList.innerHTML += `<div><p>${colaboratorEmail.value} - ${colaboratorNotify.checked ? 'Will': 'Will not'} notify</p></div>`;
+    colaboratorEmail.value = '';
+    colaboratorNotify.checked = false;
+  });
 })();
