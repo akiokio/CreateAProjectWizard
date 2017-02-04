6922111ff19882539392d4306a8b4e80a63e890a
akiokio <akio.xd@gmail.com>
2017-02-03 21:45:13 -0800

    Add form names and remove drag from siema

--- js/global.js
+++ js/global.js
@@ -1,13 +1,15 @@
 (() => {
   const progress = document.querySelector('.progress');
-  const mySiema = new Siema({
-    draggable: false,
-    startIndex: 1,
-  });
 
   const updateProgress = (currentIndex) => {
     progress.value = (currentIndex / 5) * 100;
   };
+  const mySiema = new Siema({
+    draggable: false,
+    startIndex: 1,
+    initCallback: updateProgress,
+    dragCallback: updateProgress,
+  });
   document.querySelector('.prev').addEventListener('click', () => mySiema.prev(updateProgress));
   document.querySelector('.next').addEventListener('click', () => mySiema.next(updateProgress));
 })();
