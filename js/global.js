3d08729a9c90de530b26970e4d1b92a25be81439
akiokio <akio.xd@gmail.com>
2017-02-03 20:48:47 -0800

    Add update progress bar on siema callbacks

--- js/global.js
+++ js/global.js
@@ -1,8 +1,13 @@
 (() => {
+  const progress = document.querySelector('.progress');
   const mySiema = new Siema({
     draggable: false,
     startIndex: 1,
   });
-  document.querySelector('.prev').addEventListener('click', () => mySiema.prev());
-  document.querySelector('.next').addEventListener('click', () => mySiema.next());
+
+  const updateProgress = (currentIndex) => {
+    progress.value = (currentIndex / 5) * 100;
+  };
+  document.querySelector('.prev').addEventListener('click', () => mySiema.prev(updateProgress));
+  document.querySelector('.next').addEventListener('click', () => mySiema.next(updateProgress));
 })();
