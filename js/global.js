c5fd588eb0e2971851f9836b6ea4d4293f242d52
akiokio <akio.xd@gmail.com>
2017-02-06 19:37:01 -0800

    Add the milestones tab

--- js/global.js
+++ js/global.js
@@ -8,7 +8,7 @@
   };
   const mySiema = new Siema({
     draggable: false,
-    startIndex: 1,
+    startIndex: 2,
     initCallback: updateProgress,
     dragCallback: updateProgress,
   });
@@ -21,12 +21,37 @@
   // Add colaborator button
   const addColabButton = document.querySelector('.addColaborator');
   const colaboratorsList = document.querySelector('.colaborators-list');
-  addColabButton.addEventListener('click', () => {
+  addColabButton.addEventListener('click', (e) => {
+    e.preventDefault();
     const colaboratorEmail = document.querySelector('#newColaboratorEmail');
     const colaboratorNotify = document.querySelector('#shouldNotifyNewColaborator');
     if (colaboratorEmail.value === '') { return alert('Cannot be blank'); }
-    colaboratorsList.innerHTML += `<div><p>${colaboratorEmail.value} - ${colaboratorNotify.checked ? 'Will': 'Will not'} notify</p></div>`;
+    colaboratorsList.innerHTML += `<div>
+    <p class="vertical-center">
+      <i class="fa fa-user" aria-hidden="true"></i>&nbsp;
+      ${colaboratorEmail.value} - ${colaboratorNotify.checked ? 'Will': 'Will not'} notify
+    </p></div>`;
     colaboratorEmail.value = '';
     colaboratorNotify.checked = false;
+  });
+
+  // Add milestone button
+  const addMilestoneButton = document.querySelector('.addMilestone');
+  const milestonesList = document.querySelector('.milestone-list');
+  addMilestoneButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const milestoneName = document.querySelector('#milestoneName');
+    const milestoneDueDate = document.querySelector('#milestoneDueDate');
+    const milestoneDescription = document.querySelector('#milestoneDescription');
+    if (milestoneName.value === ''
+        || milestoneDueDate.value === ''
+        || milestoneDescription.value === '') { return alert('Cannot be blank'); }
+    milestonesList.innerHTML +=`<div>
+      <p class="vertical-center"><i class="fa fa-bolt" aria-hidden="true"></i>&nbsp;${milestoneName.value}&nbsp;-&nbsp;
+      <i class="fa fa-calendar" aria-hidden="true"></i>&nbsp;${milestoneDueDate.value}</p>
+    </div>`;
+    milestoneName.value = '';
+    milestoneDueDate.value = '';
+    milestoneDescription.value = '';
   });
 })();
